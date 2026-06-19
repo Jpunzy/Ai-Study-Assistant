5b53798e1585770221a5afdf8090556f43884384
PORAMIN DUANGKERD <67160261@go.buu.ac.th>
2026-06-19 20:29:51 +0700

    feat: add Prisma module and service, update AppModule and QuizService

--- AI Assistant Study backend/study-assistant-backend/src/quiz/quiz.service.ts
+++ AI Assistant Study backend/study-assistant-backend/src/quiz/quiz.service.ts
@@ -1,4 +1,11 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class QuizService {}
+export class QuizService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly prisma: PrismaService,
+  ) {}
+}
